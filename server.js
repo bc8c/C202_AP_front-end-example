77943c4877c4df6cb271fe0ca5525047956e2f59
bc8c <bc8c@naver.com>
2020-09-07 00:08:38 +0900

    -Create basic server executable file (server.js) -Create module installation configuration file (package.json)

---
+++ server.js
@@ -0,0 +1,27 @@
+// ExpressJS Setup
+const path = require('path');
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+
+// Hyperledger Bridge
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// use static file
+app.use(express.static(path.join(__dirname, 'views')));
+
+// configure app to use body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// main page routing
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html');
+})
+
+// server start
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
